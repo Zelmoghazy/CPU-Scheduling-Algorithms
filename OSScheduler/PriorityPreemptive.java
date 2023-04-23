ac8a6094612612f3a0c1627b83dcfc805b71aabb
zelmoghazy <zelmoghazy@gmail.com>
2023-04-24 01:15:17 +0200

    added PriorityPreemptive

---
+++ OSScheduler/PriorityPreemptive.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.TreeSet;
+
+public class PriorityPreemptive extends Priority {
+
+    int priority = Integer.MAX_VALUE;
+
+    public PriorityPreemptive(ArrayList<Process> processes) {
+        super(processes);
+    }
+
+    @Override
+    void sort() {
+        if (sortedProcesses == null) {
+            sortedProcesses = new TreeSet<>(new Comparator<Process>() {
+                @Override
+                public int compare(Process o1, Process o2) {
+                    int result = o1.getPriority() - o2.getPriority();
+                    if (result == 0) {
+                        result = o1.getArrivalTime() - o2.getArrivalTime();
+                    }
+                    if (result == 0) {
+                        result = o1.getPID() - o2.getPID();
+                    }
+                    return result;
+                }
+            });
+        } else {
+            sortedProcesses.clear();
+        }
+
+        for (Process p : processes) {
+            if (p.getArrivalTime() <= time && p.getPriority() <= priority && p.remainingTime > 0) {
+                priority = p.getPriority();
+                sortedProcesses.add(p);
+            }
+        }
+    }
+
+    @Override
+    void schedule() {
+        ganttChart = new GanttChart(processes);
+        time = 0;
+        totalWaitingTime = 0;
+        averageWaitingTime = 0;
+        totalTurnAroundTime = 0;
+        averageTurnAroundTime = 0;
+        sort();
+        do {
+            ganttChart.Schedule(sortedProcesses.first());
+            sortedProcesses.first().remainingTime--;
+            if (sortedProcesses.first().remainingTime == 0) {
+                priority = Integer.MAX_VALUE;
+            }
+            time++;
+            sort();
+        } while (!sortedProcesses.isEmpty());
+        ganttChart.calculateWaitingTime();
+        ganttChart.printGanttChart();
+        averageWaitingTime = ganttChart.getTotalWaitingTime() / processes.size();
+        averageTurnAroundTime = ganttChart.getTotalTurnAroundTime() / processes.size();
+    }
+}
