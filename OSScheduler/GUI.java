178168685fa038e122bdc4eebfda51f1ca2efb31
zelmoghazy <zelmoghazy@gmail.com>
2023-04-26 01:22:58 +0200

    GUI Dynamic Gantt Chart

--- OSScheduler/GUI.java
+++ OSScheduler/GUI.java
@@ -1,20 +1,30 @@
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
-public class GUI extends Application
-{
+public class GUI extends Application {
+    private int stop_value;
+    Map<Process, Integer> burst_count = new HashMap<>();
+
     @Override
-    public void start(Stage s1) 
-    {
+    public void start(Stage s1) {
         HBox root = new HBox(0); // 10 pixels of spacing between rectangles
         ArrayList<Process> processes = new ArrayList<>();
         processes.add(new Process(1, 5, 0));
@@ -23,42 +33,125 @@ public class GUI extends Application
         processes.add(new Process(4, 2, 3));
         processes.add(new Process(5, 3, 4));
 
-        RoundRobin rr = new RoundRobin(processes, 2);
-        ArrayList<GanttChartBar> ganttChartBars = rr.getGanttChartBars();
-        
-        int index = 1;
-        for (GanttChartBar ganttChartBar : ganttChartBars) 
-        {
-            for (int i = 0; i < ganttChartBar.getEndTime() - ganttChartBar.getStartTime(); i++) 
-            {
-                StackPane stack = new StackPane();
-                Rectangle rectangle = createRectangle(ganttChartBar.getProcess().getPID());
-                Text h = new Text("P"+ ganttChartBar.getProcess().getPID() + "\n" + index);
-                stack.getChildren().addAll(rectangle, h);
-                root.getChildren().add(stack);
-                index++;
-            }
-        }
-        // Create a scene with the layout pane as the root node
-        Scene scene = new Scene(root, 800, 350); // Adjust scene width to fit both rectangles
+        Button button = new Button("Stop");
+        Button button2 = new Button("Play");
 
-        // Set the scene on the primary stage and show it
+        Timeline dynamic_gunt = new Timeline();
+
+        int time_quantum = 2;
+        RoundRobin rr = new RoundRobin(processes, time_quantum);
+        ArrayList<GanttChartBar> ganttChartBars = rr.getGanttChartBars();
+
+        for (GanttChartBar ganttChartBar : ganttChartBars) {
+            burst_count.put(ganttChartBar.getProcess(), ganttChartBar.getProcess().getBurstTime());
+        }
+        stop_value = 0;
+        int index = 0;
+
+        for (GanttChartBar ganttChartBar : ganttChartBars) {
+            for (int i = 0; i < ganttChartBar.getDuration(); i++) {
+                index++;
+                if (index < stop_value) {
+                    continue;
+                } else {
+                    StackPane stack = new StackPane();
+                    Rectangle rectangle = createRectangle(ganttChartBar.getProcess().getPID());
+                    Text h = new Text(burst_count.get(ganttChartBar.getProcess()).toString() + "\n" + index);
+                    // Text h = new Text("P"+ ganttChartBar.getProcess().getPID() + "\n" + index);
+                    burst_count.put(ganttChartBar.getProcess(), burst_count.get(ganttChartBar.getProcess()) - 1);
+                    KeyFrame kf = new KeyFrame(Duration.seconds(index + 1), actionEvent -> {
+
+                        stack.getChildren().addAll(rectangle, h);
+                        root.getChildren().add(stack);
+
+                    });
+                    dynamic_gunt.getKeyFrames().add(kf);
+                }
+            }
+
+        }
+        dynamic_gunt.play();
+
+        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e) {
+                stop_value += (int) dynamic_gunt.getCurrentTime().toSeconds();
+                dynamic_gunt.pause();
+                dynamic_gunt.getKeyFrames().clear();
+            }
+        };
+
+        button.setOnAction(event);
+
+        EventHandler<ActionEvent> event2 = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e) {
+                // Timeline dynamic_gunt = new Timeline();
+                processes.add(new Process(6, 4, 3));
+                RoundRobin rr = new RoundRobin(processes, time_quantum);
+                ArrayList<GanttChartBar> ganttChartBars = rr.getGanttChartBars();
+
+                for (GanttChartBar ganttChartBar : ganttChartBars) {
+                    burst_count.put(ganttChartBar.getProcess(), ganttChartBar.getProcess().getBurstTime());
+                }
+
+                int elapsed = 1;
+                for (GanttChartBar ganttChartBar : ganttChartBars) {
+                    if (elapsed >= stop_value){
+                        break;
+                    }
+                    for (int i = 0; i < ganttChartBar.getDuration(); i++) {
+                        if (elapsed >= stop_value) {
+                            break;
+                        }
+                        burst_count.put(ganttChartBar.getProcess(), burst_count.get(ganttChartBar.getProcess()) - 1);
+                        elapsed++;
+                    }
+                }
+
+                int index = 0;
+                for (GanttChartBar ganttChartBar : ganttChartBars) {
+                    for (int i = 0; i < ganttChartBar.getDuration(); i++) {
+                        // System.out.println(ganttChartBar.getDuration());
+                        index++;
+                        if (index < stop_value) {
+                            continue;
+                        } else {
+                            StackPane stack = new StackPane();
+                            Rectangle rectangle = createRectangle(ganttChartBar.getProcess().getPID());
+                            Text h = new Text(burst_count.get(ganttChartBar.getProcess()).toString() + "\n" + index);
+                            // Text h = new Text("P"+ ganttChartBar.getProcess().getPID() + "\n" + index);
+                            burst_count.put(ganttChartBar.getProcess(),
+                                    burst_count.get(ganttChartBar.getProcess()) - 1);
+                            KeyFrame kf = new KeyFrame(Duration.seconds(index - stop_value + 1), actionEvent -> {
+                                stack.getChildren().addAll(rectangle, h);
+                                root.getChildren().add(stack);
+                            });
+                            dynamic_gunt.getKeyFrames().add(kf);
+                        }
+                    }
+                }
+                dynamic_gunt.playFromStart();
+            }
+        };
+
+        button2.setOnAction(event2);
+
+        root.getChildren().addAll(button, button2);
+        Scene scene = new Scene(root, 800, 350);
         s1.setScene(scene);
         s1.show();
     }
+
     public static void main(String[] args) {
         launch(args);
     }
-    private Rectangle createRectangle(int i) 
-    {
+
+    private Rectangle createRectangle(int i) {
         double width = 25; // Width between 100 and 200 pixels
         double height = 50; // Height between 50 and 100 pixels
-        Random random = new Random(i);
+        Random random = new Random(i + 2);
         Color color = Color.rgb(random.nextInt(256), random.nextInt(256), random.nextInt(256));
         Rectangle rectangle = new Rectangle(width, height, color);
         return rectangle;
     }
 
 }
-    
-
