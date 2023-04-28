2c5c901c001ac2e868d21fd9252fab5b54884a75
Abdelrhman Hamdy <abdelrhmanhamdy023@gmail.com>
2023-04-28 19:38:47 +0300

    adding comments to implement code

--- OSScheduler/addLiveProcessController.java
+++ OSScheduler/addLiveProcessController.java
@@ -11,53 +11,77 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+//controller for add process in live
 public class addLiveProcessController {
-	
-	@FXML
+
+    @FXML
     private Button addProcee_btn;
 
     @FXML
-    TextField burstTime_Text, PID,priority_Text;
+    TextField burstTime_Text, PID, priority_Text;
 
     @FXML
     private Button cancel_btn;
 
     @FXML
     private AnchorPane rootPane;
-    
+
     @FXML
     Label PriorityLabel;
-    
+
     Stage stage;
-    
+
     private Process newProcess;
+    // a static object to add processes to the same object every time this
+    // controller is invocated
     static LiveSceneController secondController;
-    
+
+    // event to add new process
     public void add(ActionEvent event) {
-    	if (burstTime_Text.getText().isEmpty()) {
+        // handle if the user forgeted to enter data in text fields
+        if (burstTime_Text.getText().isEmpty()) {
             errorDialog("Burst Time text field can't be empty.");
             return;
         }
-    	if (PID.getText().isEmpty()) {
-    		errorDialog("Process ID text field can't be empty.");
-    		return;
-    	}
-    	try {
-    		newProcess = new Process(Integer.parseInt(PID.getText()), Integer.parseInt(burstTime_Text.getText()), 0 , Integer.parseInt(priority_Text.getText()));
-		} catch (Exception e) {
-			errorDialog("process ID, burst time, and priority must be Integers");
-			return;
-		}
-    	
-    	secondController.addProccessToMyList(newProcess);
+        if (PID.getText().isEmpty()) {
+            errorDialog("Process ID text field can't be empty.");
+            return;
+        }
+
+        // handle if user entered non-integer value in any data field
+        try {
+            Integer.parseInt(PID.getText());
+        } catch (Exception e) {
+            errorDialog("process ID must be Integer");
+            return;
+        }
+        try {
+            Integer.parseInt(burstTime_Text.getText());
+        } catch (Exception e) {
+            errorDialog("burst time must be Integer");
+            return;
+        }
+        try {
+            Integer.parseInt(priority_Text.getText());
+        } catch (Exception e) {
+            errorDialog("priority must be Integer");
+            return;
+        }
+
+        // create a new process with data entered
+        newProcess = new Process(Integer.parseInt(PID.getText()), Integer.parseInt(burstTime_Text.getText()), 0,
+                Integer.parseInt(priority_Text.getText()));
+
+        // add process to the live scene
+        secondController.addProccessToMyList(newProcess);
     }
 
     @FXML
     void Cancel(ActionEvent event) {
-		stage = (Stage) rootPane.getScene().getWindow();
-		stage.close();
+        stage = (Stage) rootPane.getScene().getWindow();
+        stage.close();
     }
-    
+
     private void errorDialog(String msg) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Error Dialog");
@@ -66,17 +90,18 @@ public class addLiveProcessController {
 
         alert.showAndWait();
     }
-    
+
     static void setController(LiveSceneController con) {
-    	secondController = con;
+        secondController = con;
     }
-    
+
+    // show/hide priority text and label depending on the algorithm
     public void initLabelAndText(boolean visible) {
-    	PriorityLabel.setText("Priority");
-    	PriorityLabel.setVisible(visible);
-		PriorityLabel.setDisable(!visible);
-		priority_Text.setVisible(visible);
-		priority_Text.setDisable(!visible);
-	}
-  
+        PriorityLabel.setText("Priority");
+        PriorityLabel.setVisible(visible);
+        PriorityLabel.setDisable(!visible);
+        priority_Text.setVisible(visible);
+        priority_Text.setDisable(!visible);
+    }
+
 }
